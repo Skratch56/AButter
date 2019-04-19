bf7758f64268c23dcf0dd31a5671cf15bf339835
Thubalethu Mambane <skratch56@gmail.com>
2019-04-19 14:26:20 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ Route::get('install/pre-installation', 'InstallController@preInstallation');
 Route::get('install/configuration', 'InstallController@getConfiguration');
 Route::post('install/configuration', 'InstallController@postConfiguration');
 Route::get('install/complete', 'InstallController@complete');
+Route::get('/config-cache', function() {
+    Artisan::call('config:cache');
+    return '<h1>Config cache cleared successfully</h1>';
+});

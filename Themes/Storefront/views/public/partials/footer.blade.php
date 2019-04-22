d1a1f55d44cc8e6e2c9675e0a974f0905c3ae228
Thubalethu Mambane <skratch56@gmail.com>
2019-04-22 21:07:31 +0200

    first commit

--- Themes/Storefront/views/public/partials/footer.blade.php
+++ Themes/Storefront/views/public/partials/footer.blade.php
@@ -19,24 +19,15 @@
                         <div class="contact">
                             <ul class="list-inline">
                                 @if (setting('store_phone'))
-                                    <li>
-                                        <i class="fa fa-phone-square" aria-hidden="true"></i>
-                                        <span class="contact-info">{{ setting('store_phone') }}</span>
-                                    </li>
+                                    <li><i class="fa fa-phone-square" aria-hidden="true"></i>{{ setting('store_phone') }}</li>
                                 @endif
 
                                 @if (setting('store_email'))
-                                    <li>
-                                        <i class="fa fa-envelope-o" aria-hidden="true"></i>
-                                        <span class="contact-info">{{ setting('store_email') }}</span>
-                                    </li>
+                                    <li><i class="fa fa-envelope-o" aria-hidden="true"></i>{{ setting('store_email') }}</li>
                                 @endif
 
                                 @if (setting('storefront_footer_address'))
-                                    <li>
-                                        <i class="fa fa-location-arrow" aria-hidden="true"></i>
-                                        <span class="contact-info">{{ setting('storefront_footer_address') }}</span>
-                                    </li>
+                                    <li><i class="fa fa-location-arrow" aria-hidden="true"></i>{{ setting('storefront_footer_address') }}</li>
                                 @endif
                             </ul>
                         </div>
@@ -44,7 +35,7 @@
                 </div>
 
                 <div class="col-md-8">
-                    <div class="col-sm-6">
+                    <div class="col-md-6">
                         <div class="row">
                             <div class="links">
                                 <div class="mobile-collapse">
@@ -66,7 +57,7 @@
                     </div>
 
                     @if ($footerMenu->isNotEmpty())
-                        <div class="col-sm-6">
+                        <div class="col-md-6">
                             <div class="row">
                                 <div class="links">
                                     <div class="mobile-collapse">
@@ -99,7 +90,7 @@
         @endif
     </div>
 
-    <div class="footer-bottom p-tb-20 clearfix">
+    <div class="footer-bottom bg-black-dark p-tb-20 clearfix">
         <div class="container">
             <div class="copyright text-center">
                 {!! $copyrightText !!}

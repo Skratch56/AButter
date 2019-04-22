3dac892b2b2e8fcab9b6594516792f112ffb82ca
Thubalethu Mambane <skratch56@gmail.com>
2019-04-22 20:59:48 +0200

    first commit

--- Themes/Storefront/views/public/home/index.blade.php
+++ Themes/Storefront/views/public/home/index.blade.php
@@ -4,34 +4,19 @@
 
 @section('content')
     @unless (is_null($slider))
-        @if (storefront_layout() === 'default')
-            <div class="col-lg-offset-0 col-md-offset-0">
-                <div class="row">
-                    @include('public.home.sections.slider')
-                </div>
-            </div>
-            <div class="clearfix"></div>
-        @elseif (storefront_layout() === 'slider_with_banners')
+        <br><br>
+        <div class="col-lg-offset-0 col-md-offset-0">
             <div class="row">
-                <div class="col-md-9">
-                    @include('public.home.sections.slider')
-                </div>
-
-                <div class="col-md-3 hidden-sm hidden-xs">
-                    @include('public.home.sections.slider_banners')
-                </div>
+                @include('public.home.sections.slider')
             </div>
-        @endif
+        </div>
+        <div class="clearfix"></div>
     @endunless
 
     @include('public.partials.notification')
 
     @if (setting('storefront_features_section_enabled'))
         @include('public.home.sections.features')
-    @endif
-
-    @if (setting('storefront_banner_section_1_enabled'))
-        @include('public.home.sections.banner_section_1')
     @endif
 
     @if (setting('storefront_product_carousel_section_enabled'))
@@ -45,10 +30,6 @@
         @include('public.home.sections.recent_products')
     @endif
 
-    @if (setting('storefront_banner_section_2_enabled'))
-        @include('public.home.sections.banner_section_2')
-    @endif
-
     @if (setting('storefront_three_column_vertical_product_carousel_section_enabled'))
         @include('public.home.sections.three_column_vertical_product_carousel')
     @endif
@@ -58,10 +39,6 @@
             'title' => setting('storefront_landscape_products_section_title'),
             'products' => $landscapeProducts
         ])
-    @endif
-
-    @if (setting('storefront_banner_section_3_enabled'))
-        @include('public.home.sections.banner_section_3')
     @endif
 
     @if (setting('storefront_product_tabs_section_enabled'))
